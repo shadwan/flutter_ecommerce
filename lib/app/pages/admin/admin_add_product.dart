ff92e58a5c8060786b5f8ae4c4553846b19812c7
Shadman Sakib <shadman@felicity.digital>
2023-03-04 03:22:06 +0600

    Add Admin Fields

---
+++ lib/app/pages/admin/admin_add_product.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AdminAddProductPage extends ConsumerStatefulWidget {
+  const AdminAddProductPage({super.key});
+
+  @override
+  ConsumerState<AdminAddProductPage> createState() =>
+      _AdminAddProductPageState();
+}
+
+class _AdminAddProductPageState extends ConsumerState<AdminAddProductPage> {
+  final titleTextEditingController = TextEditingController();
+  final priceEditingController = TextEditingController();
+  final descriptionEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            CustomInputFieldFb1(
+              inputController: titleTextEditingController,
+              hintText: 'Product Name',
+              labelText: 'Product Name',
+            ),
+            const SizedBox(height: 15),
+            CustomInputFieldFb1(
+              inputController: descriptionEditingController,
+              hintText: 'Product Description',
+              labelText: 'Product Description',
+            ),
+            const SizedBox(height: 15),
+            CustomInputFieldFb1(
+              inputController: priceEditingController,
+              hintText: 'Price',
+              labelText: 'Price',
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () => _addProduct,
+              child: const Text("Add Product"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _addProduct() {}
+}
+
+class CustomInputFieldFb1 extends StatelessWidget {
+  final TextEditingController inputController;
+  final String hintText;
+  final Color primaryColor;
+  final String labelText;
+
+  const CustomInputFieldFb1(
+      {Key? key,
+      required this.inputController,
+      required this.hintText,
+      required this.labelText,
+      this.primaryColor = Colors.indigo})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            offset: const Offset(12, 26),
+            blurRadius: 50,
+            spreadRadius: 0,
+            color: Colors.grey.withOpacity(.1)),
+      ]),
+      child: TextField(
+        controller: inputController,
+        onChanged: (value) {
+          //Do something wi
+        },
+        keyboardType: TextInputType.emailAddress,
+        style: const TextStyle(fontSize: 16, color: Colors.black),
+        decoration: InputDecoration(
+          labelText: labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.grey.withOpacity(.75)),
+          fillColor: Colors.transparent,
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+          border: UnderlineInputBorder(
+            borderSide:
+                BorderSide(color: primaryColor.withOpacity(.1), width: 2.0),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: primaryColor, width: 2.0),
+          ),
+          errorBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.red, width: 2.0),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide:
+                BorderSide(color: primaryColor.withOpacity(.1), width: 2.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
