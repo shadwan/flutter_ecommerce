4be51697040374b6e79ac845e5ee383b691c96c1
Shadman Sakib <shadman@felicity.digital>
2023-03-20 00:33:07 +0600

    User Bag

--- lib/app/providers.dart
+++ lib/app/providers.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/services/firestore_service.dart';
 import 'package:ecommerce/services/storage_service.dart';
+import 'package:ecommerce/view_models/bag_view_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -30,4 +31,8 @@ final storageProvider = Provider<StorageService?>((ref) {
     return StorageService(uid: uid);
   }
   return null;
+});
+
+final bagProvider = ChangeNotifierProvider<BagViewModel>((ref) {
+  return BagViewModel();
 });
